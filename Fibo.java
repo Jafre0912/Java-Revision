fe451e035c12beae18bb7d0fc7dd1e2985ce4b5c
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-01-10 14:49:24 +0545

    Add files via upload

---
+++ Fibo.java
@@ -0,0 +1,14 @@
+//fibonaaci number a number which is the sum of the previous 2 numbers i.e. 0 1 1 2 3 5 8 13 21 34
+public class Fibo {
+    public static void main(String[] args) {
+        int ans = fibo(5);
+        System.out.println(ans);
+    }
+    static int fibo(int n){
+        if(n<=0){
+            return 0;
+        }else{
+            return fibo(n-1)+fibo(n-2);
+        }
+    }
+}
