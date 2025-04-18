710e124e82d0358b8cb59e3240f8f73225ef68dd
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-04-18 21:19:34 +0000

    ArrayOperation

--- DSA_Learning/CountArr.java
+++ DSA_Learning/CountArr.java
@@ -2,20 +2,50 @@
 
 class Main{
     public static void main(String[] args) {
-        int [] arr = new int[5];
+        int [] arr = new int[7];
         arr[0] = 4; 
-        arr[1] = 4;
+        arr[1] = 3;
         arr[2] = 3;
         arr[3] = 4;
         arr[4] = 5;
-        int x = 4; // here initilizing for 4 that how many times 4 occurs in the array 
-        System.out.println("Count of X is: "+ countOccurences(arr, x)); 
+        arr[5] = 6;
+        arr[6] = 7;
+        int x = 3; // here initilizing for 4 that how many times 4 occurs in the array 
+        //System.out.println("Count of X is: "+ countOccurences(arr, x));
+        // System.out.println(lastOccurences(arr, x));
+        System.out.println(greaterElement(arr, x));
     }
     static int countOccurences(int [] arr, int x){
         int count = 0;
         //x = 5;
         for(int i = 0; i<arr.length; i++){
             if(arr[i]==x){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    ////count the last index occurences of a particular element x
+    static int lastOccurences(int arr[], int x){
+        int lastIndex = -1; // if last index not are in array will return this 
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i]==x){
+                lastIndex = i;
+            }
+        }
+        return lastIndex;
+    }
+
+
+
+
+    //Count the number of elements strictly greater that value x
+    static int greaterElement(int [] arr, int x){
+        int count = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i]>x){
                 count++;
             }
         }
