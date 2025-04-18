638081fa2cc16507cc8fe3875da4e8fdb508997f
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-04-18 22:07:18 +0000

    ArrOperation

--- DSA_Learning/CountArr.java
+++ DSA_Learning/CountArr.java
@@ -13,7 +13,9 @@ class Main{
         int x = 3; // here initilizing for 4 that how many times 4 occurs in the array 
         //System.out.println("Count of X is: "+ countOccurences(arr, x));
         // System.out.println(lastOccurences(arr, x));
-        System.out.println(greaterElement(arr, x));
+       System.out.println(greaterElement(arr, x));
+       int [] arr1 = {1,3,4,5,6,2,10};
+       System.out.println(isSorted(arr1));
     }
     static int countOccurences(int [] arr, int x){
         int count = 0;
@@ -50,6 +52,19 @@ class Main{
             }
         }
         return count;
+    }
+
+
+    //checking the array is sorted or not 
+    static boolean isSorted(int [] arr1){
+        boolean check = true;
+        for(int i = 1; i<arr1.length; i++){
+            if(arr1[i]<arr1[i-1]){ //not sorted logic if arr1[i]>arr1[i-1] this is sorted array logic
+                check =  false;
+                break;
+            }
+        }
+        return check;
     }
 
 }
