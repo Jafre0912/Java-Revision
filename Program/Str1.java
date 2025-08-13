403b2ce6dd81c13b04aa23d7b91c6ec55561daf4
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-08-13 21:01:09 +0000

    Arr

--- Program/Str1.java
+++ Program/Str1.java
@@ -10,11 +10,11 @@ public class Str1 {
         String s3 = new String("jafre alam");
         char[] c = {'J','A','F','R','E'};
         byte[]b = {66,67,68,69,70};
-        String s4 = new String(c,1,3);
-        String s5 = new String(b,2,3);
+        String s4 = new String(c,1,3); // here mkaing string from the char array
+        String s5 = new String(b,2,3); // here making the string from the byte array
         System.out.println(s4);
-        System.out.println(s1==s2);
-        System.out.println(s1==s3);
+        System.out.println(s1==s2); //same 
+        System.out.println(s1==s3); //different
         System.out.println(s5);
     }
 }
--- Program/StringMethod2.java
+++ Program/StringMethod2.java
@@ -4,15 +4,15 @@ public class StringMethod2 {
     public static void main(String[] args) {
         String str = "Mr. Shahrukh khan";
         String str1 = "www.lpu.co.in";
-        System.out.println(str.startsWith("Sh"));
-        System.out.println(str.endsWith("khan"));
-        System.out.println(str.charAt(5));
-        for(int i = 0; i<str.length();i++){
-            System.out.print(str.charAt(i));
-        }  
-        System.out.println(str1.indexOf('.'));
-        System.out.println(str1.indexOf('.',7 ));
-        System.out.println(str1.lastIndexOf("co"));
+        System.out.println(str.startsWith("Sh")); //this string not starting with Sh it is starting with M, Mr, Mr.
+        System.out.println(str.endsWith("khan")); //yes it is ending with khan
+        System.out.println(str.charAt(5)); //charAt(str.length) character at 5 index
+       // for(int i = 0; i<str.length();i++){
+            //System.out.print(str.charAt(i));
+       // }  
+        System.out.println(str1.indexOf('.')); //Finds the first position (index) of the character '.' in the string.
+        System.out.println(str1.indexOf('.',7 )); //Same as above, but starts searching from index 7 instead of the beginning.Skips everything before index 7.
+        System.out.println(str1.lastIndexOf("co")); //Finds the last occurrence of "co" in the string. Returns the starting index of the last match.Returns -1 if not found.
         System.out.println(str1.lastIndexOf('w',1 ));
     }
 }
