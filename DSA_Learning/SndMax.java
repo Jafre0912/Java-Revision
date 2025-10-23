52c6ef25081633548dc31eabd20ba6e3dccb15a3
JAFRE_ALAM <112960806+Jafre0912@users.noreply.github.com>
2025-10-23 21:36:30 +0000

    2md largest element in the array

--- DSA_Learning/SndMax.java
+++ DSA_Learning/SndMax.java
@@ -2,17 +2,24 @@ package DSA.Array;
 
 public class SndMax {
     public static void main(String[] args) {
-        int [] arr = {11,23,4,5,56,7,8,9,100};
-        int max1 ,max2;
-        max1=max2=arr[0];
+        int [] arr = {11,23,4,5,56,7,8,9,100,100};
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        if(arr==null || arr.length<2){
+            throw new IllegalArgumentException("array must have atleast 2 elements");
+        }
         for(int i = 0; i<arr.length; i++){
             if(arr[i]>max1){
                 max2 = max1;
                 max1 = arr[i];
             }
-            else if(arr[i]>max2){
+            else if(arr[i]>max2 && arr[i] != max1){
                 max2=arr[i];
             }
+        }
+        if(max2==Integer.MIN_VALUE){
+            throw new IllegalArgumentException("ARR MUST HAVE 2 unique elements");
         }
         System.out.println(max2);
     }
